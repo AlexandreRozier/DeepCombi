5abb60052ae53cb89bf3b97f6987167efe161187
hx <alexandre.rozier@lilo.org>
2019-09-10 14:49:32 +0200

    Finished per chromo hpopt

--- tests/test_lotr.py
+++ tests/test_lotr.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import ParameterGrid
 from parameters_complete import random_state, REAL_DATA_DIR, DATA_DIR, nb_of_nodes
 from keras.utils import to_categorical
 from sklearn.model_selection import StratifiedShuffleSplit
-
+import keras.backend as K
 from Indices import Indices
 
 
@@ -32,12 +32,12 @@ class TestLOTR(object):
             
 
             f = h5py.File(os.path.join(DATA_DIR,'chromo_{}.mat'.format(chrom)),'r')
-            data = f.get('X')[:].T[:300] # TODO remove me
+            data = f.get('X')[:].T                                                                      
             data = data.reshape(data.shape[0],-1,3)                                                                                                                                                                                                                                                                                                                                                              
             fm = char_matrix_to_featmat(data, '3d')
             n_subjects, n_snps, _ = fm.shape
 
-            labels = scipy.io.loadmat(os.path.join(DATA_DIR,'labels.mat'))['y'][0][:300] # TODO remove me
+            labels = scipy.io.loadmat(os.path.join(DATA_DIR,'labels.mat'))['y'][0]
             labels_0based = (labels+1)/2
             labels_cat = to_categorical(labels_0based)
 
@@ -48,7 +48,7 @@ class TestLOTR(object):
 
             params_space = {
                 'n_snps':[fm.shape[1]],
-                'epochs': [4],
+                'epochs': [1000],
                 'patience': [10,30,50,100],
                 'factor':np.linspace(0.1,0.8,9),
                 'dropout_rate':np.linspace(0.1,0.5,5),
@@ -58,12 +58,13 @@ class TestLOTR(object):
             }
 
             grid = ParameterGrid(params_space)
-            BUDGET = 2
+            BUDGET = 100
             print("Testing {} % of the hp space".format(BUDGET/len(grid)*100))
             grid = random_state.choice(list(grid), BUDGET)
 
             best_acc = 0
             for g in grid:
+                K.clear_session()
                 model = create_montaez_dense_model_2(g)
 
                 history = model.fit(x=fm[idx.train],
@@ -90,6 +91,6 @@ class TestLOTR(object):
                 if val_acc > best_acc:
                     best_acc = val_acc
                     model.save(os.path.join(REAL_DATA_DIR,'models','chrom{}'.format(chrom)))
-                    pickle.dump(g, os.path.join(REAL_DATA_DIR,'hyperparams','chrom{}'.format(chrom)))
+                    pickle.dump(g, open(os.path.join(REAL_DATA_DIR,'hyperparams','chrom{}.p'.format(chrom)),'wb'))
 
                 
