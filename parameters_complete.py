0f8645ed3c9af23a6d1c5af6b8fafb8990df6ad7
bettinanana <bettina.mieth@tu-berlin.de>
2020-11-17 11:58:33 +0100

    Update parameters_complete.py

--- parameters_complete.py
+++ parameters_complete.py
@@ -4,14 +4,14 @@ import numpy as np
 from time import time
 
 if 'ROOT_DIR' not in os.environ:
-    os.environ['ROOT_DIR'] = "/home/hx/Work/Masterarbeit/PythonImplementation"
+    os.environ['ROOT_DIR'] = "/home/bmieth/DeepCombi"
 
 if 'PREFIX' not in os.environ:
     os.environ['PREFIX'] = "default"
 
 
-disease_IDs =['CD', 'BD','CAD','HT','RA','T1D','T2D']
-diseases = ['Crohns disease','Bipolar disorder', 'Coronary artery disease','Hypertension','Rheumatoid arthritis','Type 1 Diabetes','Type 2 diabetes']
+disease_IDs = ['BD','CAD','CD', 'HT','RA','T1D','T2D']
+diseases = ['Bipolar disorder', 'Coronary artery disease','Crohns disease','Hypertension','Rheumatoid arthritis','Type 1 Diabetes','Type 2 diabetes']
      
 
 ROOT_DIR = os.environ['ROOT_DIR']
@@ -24,7 +24,11 @@ PARAMETERS_DIR = os.path.join(TEST_DIR,'parameters')
 SAVED_MODELS_DIR = os.path.join(TEST_DIR,'exported_models')
 TB_DIR = os.path.join(TEST_DIR,'exported_models')
 NUMPY_ARRAYS = os.path.join(ROOT_DIR,'numpy_arrays')
-FINAL_RESULTS_DIR = os.path.join(ROOT_DIR,'experiments','MONTAEZ_CD_C3_HP')
+FINAL_RESULTS_DIR = os.path.join(ROOT_DIR,'experiments','MONTAEZ_final')
+
+
+pvalue_threshold = 1e-2#1.1, 1e-4
+
 
 ########
 ttbr = 6
@@ -33,10 +37,24 @@ inform_snps= 20  # 15-20
 noise_snps= 10000  # 10,000, even
 n_total_snps = inform_snps + noise_snps
 top_k= 30
-real_top_k = 100
+real_top_k = 100 # This one
 
 ################################
+# DeepCOMBI postprocessing parameters
+filter_window_size_deep=  21 # 21
+real_p_pnorm_filter_deep = 2	# 2
+p_svm_deep = 2	# 2
+real_top_k_deep = 200	# 200
 
+# best for LRPAlpha2Beta1: 21, 2,2,200
+# best for LRPAlpha2Beta1IgnoreBias: 35, 2,1,200
+# best for LRPZPlusFast: 35, 1 ,2 , 400
+################################
+
+n_permutations = 100
+################################
+		
+		
 nb_of_jobs = 31
 
 
@@ -59,7 +77,8 @@ seed = 6666 # Satan helps us
 random_state = np.random.RandomState(seed)
 
 
-filter_window_size= 35  # 35 # 1-41, odd!!!
+filter_window_size= 35  # THIS ONE
+ # 35 # 1-41, odd!!!
 #filter_window_size_mtest = 9  # 35 # 1-41, odd!!!
 
 # USED in char_matrix_to_featmat to scale the resulting feature matrix
@@ -68,11 +87,11 @@ real_pnorm_feature_scaling = 6
 
 # USED TO APPLY SCALING ON WEIGHTS DURING THE POSTPROCESSING STEP 
 p_pnorm_filter = 2 
-real_p_pnorm_filter = 2
+real_p_pnorm_filter = 2 # THIS ONE
 #real_p_pnorm_filter_mtest =6
 
 
-p_svm = 2 # Used on SVM weights before applying postprocessing function 
+p_svm = 2 # THIS ONE # Used on SVM and LRP weights before applying postprocessing function 
 svm_epsilon = 1e-3
 
 rep_inform = 1
