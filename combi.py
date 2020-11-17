d97c6ed0e662ff30150f29b39dc3147f56349d9e
bettinanana <bettina.mieth@tu-berlin.de>
2020-11-17 11:52:33 +0100

    Update combi.py

--- combi.py
+++ combi.py
@@ -3,12 +3,19 @@
 import matplotlib
 import numpy as np
 from sklearn import svm
+import pdb
 
 matplotlib.use('Agg')
 from tqdm import tqdm
 
-from helpers import chi_square, postprocess_weights
-from parameters_complete import svm_epsilon, Cs, real_Cs, random_state
+from helpers import char_matrix_to_featmat, chi_square, postprocess_weights
+from parameters_complete import FINAL_RESULTS_DIR, svm_epsilon, Cs, real_Cs, random_state
+from models import create_montaez_dense_model
+from sklearn.utils import class_weight
+import os
+from keras.callbacks import CSVLogger
+
+from math import ceil
 
 from joblib import Parallel, delayed
 import innvestigate
@@ -28,7 +35,6 @@ def combi_method(classifier,data, fm, labels, filter_window_size, pnorm_filter, 
      
     RETURNS: indices, pvalues 
     """
-    
     # SVM Step to select the most k promising SNPs
     classifier.fit(fm, labels)
     raw_weights = classifier.coef_[0] # n_snps * 3
@@ -50,9 +56,9 @@ def deepcombi_method(model, data, fm, labels, filter_window_size, pnorm_filter, 
                 layer.name = 'bla{}'.format(str(i))
             if layer.name == 'dense_3':
                 layer.name = 'bleurg{}'.format(str(i))
-
+    #pdb.set_trace()
     model = iutils.keras.graph.model_wo_softmax(model)
-    analyzer = innvestigate.analyzer.LRPAlpha1Beta0(model)
+    analyzer = innvestigate.analyzer.LRPAlpha2Beta1(model)
     raw_weights = np.absolute(analyzer.analyze(fm)).sum(0) # n_snps * 3
     
     selected_indices_sorted,_ = postprocess_weights(raw_weights, top_k, filter_window_size, psvm, pnorm_filter)
@@ -63,35 +69,80 @@ def deepcombi_method(model, data, fm, labels, filter_window_size, pnorm_filter, 
 
 
 
-def permuted_combi_method(data, fm, labels, filter_window_size, pnorm_filter, psvm, top_k, n_permutations, alpha):
+def permuted_combi_method(data, labels, real_pnorm_feature_scaling, filter_window_size, pnorm_filter, psvm, top_k, n_permutations, alpha, mode='min'):
 
     def f():
         permuted_labels = random_state.permutation(labels)
-        _, pvalues,_ = combi_method(classifier, data,  fm, permuted_labels, filter_window_size, pnorm_filter, psvm, top_k)
-        return  pvalues.min()
-    
-    min_pvalues = Parallel(n_jobs=-1, require='sharedmem')(delayed(f)() for i in tqdm(range(n_permutations)))
+        fm_2D = char_matrix_to_featmat(data, '2d', real_pnorm_feature_scaling)
 
+        _, pvalues,_ = combi_method(real_classifier, data,  fm_2D, permuted_labels, filter_window_size, pnorm_filter, psvm, top_k)
+
+        if mode=='min':
+            return  np.array(pvalues.min())
+        elif mode=='all':
+            return np.array(pvalues)
+        
+    min_pvalues = Parallel(n_jobs=1, require='sharedmem')(delayed(f)() for i in tqdm(range(n_permutations)))
+	
     # Alpha percentile of sorted p-values
-    t_star = np.quantile(min_pvalues, alpha)
+    min_pvalues_sorted =  np.sort(min_pvalues)
+    t_star = min_pvalues_sorted[max(ceil(n_permutations*alpha)-1,1)]
+    #pdb.set_trace()
+
+    #t_star = np.nanquantile(min_pvalues, alpha)
 
     return t_star
 
 
-def permuted_deepcombi_method(model, data, fm, labels, labels_cat, filter_window_size, pnorm_filter, psvm, top_k, n_permutations, alpha, mode='min'):
+def permuted_deepcombi_method(data, labels, disease_id, chrom, pvalue_threshold,real_pnorm_feature_scaling, filter_window_size, pnorm_filter, psvm, top_k, n_permutations, alpha, mode='min'):
 
     def f():
         permuted_labels = random_state.permutation(labels)
         permuted_labels_cat = to_categorical((permuted_labels+1)/2)
-        _, pvalues, _ = deepcombi_method(model, data, fm, permuted_labels, filter_window_size, pnorm_filter, psvm, top_k)
+        # pvalues
+        rpvt_pvalues = chi_square(data, permuted_labels)
+
+        # pvalue thresholding
+        valid_snps = rpvt_pvalues < pvalue_threshold
+        data_now = data[:,valid_snps,:]
+		
+		# Centering and Scaling
+        fm = char_matrix_to_featmat(data_now, '3d', real_pnorm_feature_scaling)
+        fm[fm>0]=1
+        fm[fm<0]=0.
+        mean = np.mean(np.mean(fm,0),0)
+        std = np.std(np.std(fm,0),0)
+        fm = (fm-mean)/std
+		
+        hp =  {'dropout_rate': 0.3, 'epochs': 500, 'hidden_neurons': 64, 'l1_reg': 0.001, 'l2_reg': 0.0001, 'lr': 1e-05, 'n_snps': int(fm.shape[1])} 
+		
+        # Optimal parameters for pthresh = 1e-02 
+        # hp =  {'dropout_rate': 0.3, 'epochs': 500, 'hidden_neurons': 64, 'l1_reg': 0.001, 'l2_reg': 0.0001, 'lr': 1e-05, 'n_snps': int(fm.shape[1])} 
+		
+        # Create Model
+        model = create_montaez_dense_model(hp)
+
+        # Class weights
+        y_integers = np.argmax(permuted_labels_cat, axis=1)
+        class_weights = class_weight.compute_class_weight('balanced', np.unique(y_integers), y_integers)
+        d_class_weights = dict(enumerate(class_weights))
+
+        # Train
+        model.fit(x=fm, y=permuted_labels_cat, epochs=hp['epochs'], verbose=0, class_weight=d_class_weights)		
+		
+        _, pvalues, _ = deepcombi_method(model, data_now, fm, permuted_labels, filter_window_size, pnorm_filter, psvm, top_k)
         if mode=='min':
             return  np.array(pvalues.min())
         elif mode=='all':
             return np.array(pvalues)
     
-    min_pvalues = np.array(Parallel(n_jobs=-1, require='sharedmem')(delayed(f)() for i in tqdm(range(n_permutations)))).flatten()
+	#, require='sharedmem'
+    min_pvalues = np.array(Parallel(n_jobs=1)(delayed(f)() for i in tqdm(range(n_permutations)))).flatten()
 
     # Alpha percentile of sorted p-values
-    t_star = np.quantile(min_pvalues, alpha)
+    min_pvalues_sorted =  np.sort(min_pvalues)
+    t_star = min_pvalues_sorted[max(ceil(n_permutations*alpha)-1,1)]
+
+    #t_star = np.nanquantile(min_pvalues, alpha)
 
     return t_star
