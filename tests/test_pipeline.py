97f92e04f0ac4f60cf26b8d4abbe1ad6547b6f88
bettinanana <bettina.mieth@tu-berlin.de>
2020-11-17 13:22:42 +0100

    Update test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,11 +8,12 @@ import numpy as np
 import pandas as pd
 from keras.models import load_model
 from tqdm import tqdm
+import pdb
 
 from combi import deepcombi_method, combi_method, real_classifier
-from helpers import char_matrix_to_featmat, postprocess_weights_without_avg, moving_average
+from helpers import char_matrix_to_featmat, postprocess_weights_without_avg, moving_average, chi_square
 from parameters_complete import real_pnorm_feature_scaling, real_p_pnorm_filter, \
-    p_svm, real_top_k, FINAL_RESULTS_DIR, filter_window_size, disease_IDs
+    p_svm, real_top_k, FINAL_RESULTS_DIR, filter_window_size, disease_IDs, pvalue_threshold, filter_window_size_deep, real_p_pnorm_filter_deep, p_svm_deep, real_top_k_deep
 
 
 class TestPipeline(object):
@@ -22,7 +23,7 @@ class TestPipeline(object):
             scores = []
             for chrom in tqdm(range(1, 23)):
                 data = pd.read_csv(os.path.join(FINAL_RESULTS_DIR, 'csv_logs', disease, str(chrom)))
-                scores += data.tail(1)['val_acc'].values.tolist()
+                scores += data.tail(1)['categorical_accuracy'].values.tolist()
             np.save(os.path.join(FINAL_RESULTS_DIR, 'accuracies', disease, 'deepcombi'), scores)
 
     def test_save_combi_rm(self, real_genomic_data, real_labels, chrom_length):
@@ -61,13 +62,36 @@ class TestPipeline(object):
         for chromo in tqdm(range(1, 23)):
             model = load_model(os.path.join(FINAL_RESULTS_DIR, 'trained_models', disease, 'model{}.h5'.format(chromo)))
 
-            data = real_genomic_data(disease, chromo)
-            fm = char_matrix_to_featmat(data, '3d', real_pnorm_feature_scaling)
+            data = real_genomic_data(disease, chromo)		
             labels = real_labels(disease)
-            _, _, raw_rm = deepcombi_method(model, data, fm, labels, filter_window_size, real_p_pnorm_filter,
-                                              p_svm, top_k=real_top_k)
+			
+            # pvalues
+            rpvt_pvalues = chi_square(data, labels)
+
+            # pvalue thresholding
+            valid_snps = rpvt_pvalues < pvalue_threshold
+            data = data[:,valid_snps,:]
+
+            # use a different scaling - undo if you want to reproduce alexs findings
+            fm = char_matrix_to_featmat(data, '3d', real_pnorm_feature_scaling)
+            fm[fm>0]=1
+            fm[fm<0]=0.
+            mean = np.mean(np.mean(fm,0),0)
+            std = np.std(np.std(fm,0),0)
+            fm = (fm-mean)/std
             
-            total_raw_rm = np.append(total_raw_rm, raw_rm, axis=0)
+            _, _, raw_rm = deepcombi_method(model, data, fm, labels, filter_window_size_deep, real_p_pnorm_filter_deep, p_svm_deep, top_k=real_top_k_deep)
+
+            #lrp_weights_raw= raw_rm.reshape((-1))
+
+            # adjust for p-value thresholding
+            valid_inds = [i for i, x in enumerate(valid_snps) if x]
+            #valid_inds_all = np.sort(np.concatenate(([3*k + 0 for k in valid_inds],[3*k + 1 for k in valid_inds],[3*k + 2 for k in valid_inds])))
+
+            lrp_weights=np.zeros((len(rpvt_pvalues),3))
+            lrp_weights[valid_inds,:]=raw_rm
+
+            total_raw_rm = np.append(total_raw_rm, lrp_weights, axis=0)
             offset += fm.shape[1]
             del data, fm, model
 
@@ -83,6 +107,7 @@ class TestPipeline(object):
         What we call averaged weights here are the scaled + moving averaged weights.
         :return:
         """
+
 
         os.makedirs(os.path.join(FINAL_RESULTS_DIR, 'combi_avg_rm'), exist_ok=True)
         os.makedirs(os.path.join(FINAL_RESULTS_DIR, 'combi_scaled_rm'), exist_ok=True)
@@ -121,12 +146,12 @@ class TestPipeline(object):
                 combi_idx += offset
                 combi_selected_idx += combi_idx.tolist()
 
-                deepcombi_scaled_rm[offset:offset + chromo_length] = postprocess_weights_without_avg(deepcombi_raw_rm[offset:offset+chromo_length],p_svm)
+                deepcombi_scaled_rm[offset:offset + chromo_length] = postprocess_weights_without_avg(deepcombi_raw_rm[offset:offset+chromo_length],p_svm_deep)
                 deepcombi_avg_rm[offset:offset + chromo_length] = moving_average(
                     deepcombi_scaled_rm[offset:offset + chromo_length],
-                    filter_window_size,
-                    real_p_pnorm_filter)
-                deepcombi_idx = deepcombi_avg_rm[offset: offset + chromo_length].argsort()[::-1][:real_top_k]
+                    filter_window_size_deep,
+                    real_p_pnorm_filter_deep)
+                deepcombi_idx = deepcombi_avg_rm[offset: offset + chromo_length].argsort()[::-1][:real_top_k_deep]
                 deepcombi_idx += offset
                 deepcombi_selected_idx += deepcombi_idx.tolist()
 
