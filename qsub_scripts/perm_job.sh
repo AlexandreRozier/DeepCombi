d42411c34e884fdd5411f713c6def5baaadddb6d
bettinanana <bettina.mieth@tu-berlin.de>
2020-11-17 13:12:01 +0100

    Update perm_job.sh

--- qsub_scripts/perm_job.sh
+++ qsub_scripts/perm_job.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#$ -wd /home/hx/PythonImplementation
-#$ -o /home/hx/PythonImplementation/qsub_output/
-#$ -e /home/hx/PythonImplementation/qsub_errors/
-#$ -l cuda=1
-#$ -t 1-23
+#$ -wd /home/bmieth/DeepCombi
+#$ -o /home/bmieth/DeepCombi/qsub_output/
+#$ -e /home/bmieth/DeepCombi/qsub_errors/
 
 source ~/.bashrc
+
 echo "Bootstraping..."
 
 ROOT_DIR=$PWD python -m pytest -s tests/test_lotr.py::TestLOTR::test_permutations
